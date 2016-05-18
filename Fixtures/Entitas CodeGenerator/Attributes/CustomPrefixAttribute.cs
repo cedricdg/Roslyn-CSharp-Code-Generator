385af932ee39e7c525e8a9debb5920c041face7c
Cedric Deege <cedric.deege@gmx.de>
2016-05-18 22:12:46 +0200

    Add Entitas Fixtures for ComponentInfoProvider tests.

---
+++ Fixtures/Entitas CodeGenerator/Attributes/CustomPrefixAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Entitas.CodeGenerator {
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
+    public class CustomPrefixAttribute : Attribute {
+        public readonly string prefix;
+
+        public CustomPrefixAttribute(string prefix) {
+            this.prefix = prefix;
+        }
+    }
+}
---
+++ Tests/Entitas Fixtures/Components/Components.cs
@@ -0,0 +1,24 @@
+﻿using Entitas;
+
+public class ComponentB : IComponent {}
+public class ComponentC : IComponent {}
+public class ComponentD : IComponent {}
+public class ComponentE : IComponent {}
+public class ComponentF : IComponent {}
+
+public static class Component {
+    public static readonly ComponentA A = new ComponentA();
+    public static readonly ComponentB B = new ComponentB();
+    public static readonly ComponentC C = new ComponentC();
+}
+
+public static class CID {
+    public const int None = 0;
+    public const int ComponentA = 1;
+    public const int ComponentB = 2;
+    public const int ComponentC = 3;
+    public const int ComponentD = 4;
+
+    public const int NumComponents = 5;
+}
+
